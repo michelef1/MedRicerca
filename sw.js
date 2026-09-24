a72d168b10ea142c304f42ad26c689ec6c8b493a
michelef1 <michelef654321@gmail.com>
2026-09-24 17:10:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'medricerca-v1.1';
+const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
+  self.clients.claim();
+});
+// Solo file della app: rete prima, cache se offline. Le API esterne passano dirette.
+self.addEventListener('fetch', e => {
+  const u = new URL(e.request.url);
+  if (e.request.method !== 'GET' || u.origin !== location.origin) return;
+  e.respondWith(fetch(e.request).then(r => { const cp = r.clone(); caches.open(CACHE).then(c => c.put(e.request, cp)); return r; })
+    .catch(() => caches.match(e.request).then(m => m || caches.match('./index.html'))));
+});
